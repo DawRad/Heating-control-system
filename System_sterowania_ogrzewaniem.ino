ef9a4a3ecfb6502bf9887097e24fe768aa918598
DawRad <dawidissimo@gmail.com>
2022-12-18 15:39:44 +0100

    Potentiometer signal replaced with photoresistor measurment value.

--- System_sterowania_ogrzewaniem.ino
+++ System_sterowania_ogrzewaniem.ino
@@ -3,42 +3,29 @@
    Dev: Michalis Vasilakis / Date: 25/10/2014                                                   */
 
 //Constants:
-const int ledPin = 9;  //pin 9 has PWM funtion
-const int potPin = A0; //pin A0 to read analog input
+const int pResistor = A0; //pin A0 to read analog input
 
-const int ledBtn = 4;
-const int btnPin = 2;
+// const int ledBtn = 4;
+// const int btnPin = 2;
 
-//Variables:
+// //Variables:
 int value; //save analog value
-boolean ledOn;
-int btnVal;
+// boolean ledOn;
+// int btnVal;
 
 void setup(){
   //Input or output?
-  pinMode(ledPin, OUTPUT); 
-  pinMode(potPin, INPUT); //Optional
-  pinMode(ledBtn,OUTPUT);
-  pinMode(btnPin,INPUT_PULLUP);
+  pinMode(pResistor, INPUT); //Optional
+  // pinMode(ledBtn,OUTPUT);
+  // pinMode(btnPin,INPUT_PULLUP);
   Serial.begin(9600);
 
-  ledOn = false;
+  // ledOn = false;
 }
 
 void loop(){
-  value = analogRead(potPin);          //Read and save analog value from potentiometer
+  value = analogRead(pResistor);          //Read and save analog value from photoresistor
   Serial.println(value);
-  value = map(value, 0, 1023, 0, 255); //Map value 0-1023 to 0-255 (PWM)
-  analogWrite(ledPin, value);          //Send PWM value to led
-
-  btnVal = digitalRead(btnPin);
-  if(btnVal==LOW)
-  {
-    digitalWrite(ledBtn,HIGH);
-  }
-  else
-  {
-    digitalWrite(ledBtn,LOW);
-  }
+  
   delay(100);                          //Small delay  
 }
