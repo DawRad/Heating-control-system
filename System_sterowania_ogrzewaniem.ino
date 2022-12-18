4302518bc2b9be067468003a721614d507199e72
DawRad <dawidissimo@gmail.com>
2022-12-18 15:49:12 +0100

    Added displaying photoresistor measurment on LCD display.

--- System_sterowania_ogrzewaniem.ino
+++ System_sterowania_ogrzewaniem.ino
@@ -2,6 +2,12 @@
    More info and circuit schematic: http://www.ardumotive.com/arduino-tutorials/arduino-fade-led
    Dev: Michalis Vasilakis / Date: 25/10/2014                                                   */
 
+#include <Wire.h> 
+#include <LiquidCrystal_I2C.h>
+
+// Set the LCD address to 0x27 for a 16 chars and 2 line display
+LiquidCrystal_I2C lcd(0x27, 16, 2);
+
 //Constants:
 const int pResistor = A0; //pin A0 to read analog input
 
@@ -14,6 +20,13 @@ int value; //save analog value
 // int btnVal;
 
 void setup(){
+  // initialize the LCD
+	lcd.begin();
+
+	// Turn on the blacklight and print a message.
+	lcd.backlight();
+	lcd.print("Hello, world!");
+
   //Input or output?
   pinMode(pResistor, INPUT); //Optional
   // pinMode(ledBtn,OUTPUT);
@@ -26,6 +39,12 @@ void setup(){
 void loop(){
   value = analogRead(pResistor);          //Read and save analog value from photoresistor
   Serial.println(value);
-  
-  delay(100);                          //Small delay  
+
+  lcd.clear();
+  lcd.print("Light lvl:");
+  lcd.setCursor (0,1); // go to start of 2nd line
+  lcd.print(value);
+  //lcd.print(millis() / 1000);
+  delay(500);
+  //end of loopcode Robojax code for LCD with I2C
 }
