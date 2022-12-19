af950c3c9de67a385945c7a236037dfd3edecd8b
DawRad <dawidissimo@gmail.com>
2022-12-19 06:09:40 +0100

    Added temperature measurment.

--- System_sterowania_ogrzewaniem.ino
+++ System_sterowania_ogrzewaniem.ino
@@ -2,6 +2,7 @@
    More info and circuit schematic: http://www.ardumotive.com/arduino-tutorials/arduino-fade-led
    Dev: Michalis Vasilakis / Date: 25/10/2014                                                   */
 
+// #include <string.h>
 #include <Wire.h> 
 #include <LiquidCrystal_I2C.h>
 
@@ -11,29 +12,47 @@ LiquidCrystal_I2C lcd(0x27, 16, 2);
 //Constants:
 const int pResistor = A0; //pin A0 to read analog input
 const int btnPin = 2;
+const int tempPin = A2;
 
 // //Variables:
 int value; //save analog value
 boolean room_state;
 int btnVal;
+float actTemp;
+float tempAvg;
+String strRoomState;
 
 void setup(){
   // initialize the LCD
 	lcd.begin();
 
 	// Turn on the blacklight and print a message.
-	lcd.backlight();
+	// lcd.backlight();
 	lcd.print("Hello, world!");
 
   //Input or output?
   pinMode(pResistor, INPUT); //Optional
+  pinMode(tempPin, INPUT);
   pinMode(btnPin,INPUT_PULLUP);
   Serial.begin(9600);
 
   room_state = false;
+  actTemp = 0;
+  tempAvg = 0;
+  strRoomState = "empty";
 }
 
-void loop(){
+void loop()
+{
+  tempAvg = 0;
+  for (int i = 0; i < 60; i++)
+  {
+    actTemp = ((analogRead(tempPin) * 5.0) / 1024) * 10;
+    tempAvg += actTemp;
+    delay(50);
+  }
+  tempAvg = tempAvg / 60;
+
   value = analogRead(pResistor);          //Read and save analog value from photoresistor
   Serial.println(value);
 
@@ -49,7 +68,21 @@ void loop(){
   lcd.print(value);
   lcd.setCursor (0,1); // go to start of 2nd line
   lcd.print("Room: ");
-  if(room_state)  lcd.print("occupied");
-  else lcd.print("empty");  
-  delay(500);
+  if(room_state)  strRoomState = "occupied";
+  else strRoomState = "empty";  
+  lcd.print(strRoomState);
+  delay(1500);
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("Room: ");
+  lcd.print(strRoomState);
+  lcd.setCursor(0, 1);
+  lcd.print("Temp.: ");
+  lcd.print(tempAvg);    
+  delay(1500);
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("Temp.: ");
+  lcd.print(tempAvg);
+  delay(1500);
 }
